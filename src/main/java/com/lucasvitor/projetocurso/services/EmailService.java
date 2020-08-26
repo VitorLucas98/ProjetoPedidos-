d1a2f4bfd12abd88df5dacd04701b6f79d5132e0
Vitor Lucas <vitorluc.2013@gmail.com>
2020-08-26 20:37:36 -0300

    Email HTML

--- src/main/java/com/lucasvitor/projetocurso/services/EmailService.java
+++ src/main/java/com/lucasvitor/projetocurso/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.lucasvitor.projetocurso.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.lucasvitor.projetocurso.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
