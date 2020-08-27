94c23eb4eea4faa1fc7c81a20efb8a660b747713
Vitor Lucas <vitorluc.2013@gmail.com>
2020-08-26 21:30:04 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/lucasvitor/projetocurso/services/SmtpEmailService.java
+++ src/main/java/com/lucasvitor/projetocurso/services/SmtpEmailService.java
@@ -1,30 +1,26 @@
 package com.lucasvitor.projetocurso.services;
 
-<<<<<<< HEAD
+
 import javax.mail.internet.MimeMessage;
 
-=======
->>>>>>> 8ce105bc8bd1a5524e48ff85d9c74b9bc6b399fa
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-<<<<<<< HEAD
+
 import org.springframework.mail.javamail.JavaMailSender;
-=======
->>>>>>> 8ce105bc8bd1a5524e48ff85d9c74b9bc6b399fa
+
 
 public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
 	private MailSender mailSender;
-<<<<<<< HEAD
+
 	
 	@Autowired
 	private JavaMailSender javaMailSender;
-=======
->>>>>>> 8ce105bc8bd1a5524e48ff85d9c74b9bc6b399fa
 
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -34,13 +30,12 @@ public class SmtpEmailService extends AbstractEmailService {
 		mailSender.send(msg);
 		LOG.info("Email enviado");
 	}
-<<<<<<< HEAD
+
 	@Override
 	public void sendHtmlEmail(MimeMessage msg) {
 		LOG.info("Enviando email...");
 		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 	}
-=======
->>>>>>> 8ce105bc8bd1a5524e48ff85d9c74b9bc6b399fa
+
 }
