b34079e82fc44ee2e16822539a4966623537f50d
Vitor Lucas <vitorluc.2013@gmail.com>
2020-08-29 10:40:25 -0300

    Adicionando senha a Cliente

--- src/main/java/com/lucasvitor/projetocurso/dto/ClienteNewDTO.java
+++ src/main/java/com/lucasvitor/projetocurso/dto/ClienteNewDTO.java
@@ -22,6 +22,9 @@ public class ClienteNewDTO implements Serializable {
 	private String email;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
@@ -150,4 +153,11 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	} 
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
